0dcbd16753be4e4000acb060c5b4cf1942cec54a
Lars <lars@turtleeverywhere.dev>
2026-03-05 00:25:15 +0000

    feat: add Flying Turtle link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,7 @@ function Footer() {
     <footer>
       <p>{t('footer.copyright')}</p>
       <a href="/impressum" className="footer-link">{t('footer.impressum')}</a>
+      <a href="https://www.flying-turtle.de" target="_blank" rel="noopener noreferrer" className="footer-link">Flying Turtle</a>
     </footer>
   )
 }
