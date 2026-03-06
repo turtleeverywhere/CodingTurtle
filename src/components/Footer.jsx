434909e1ded9f1b0652f58f034c36b608b2349be
Lars <lars@turtleeverywhere.dev>
2026-03-06 16:14:46 +0000

    feat: add Discord community link to footer and support pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,7 @@ function Footer() {
       <p>{t('footer.copyright')}</p>
       <a href="/impressum" className="footer-link">{t('footer.impressum')}</a>
       <a href="https://www.flying-turtle.de" target="_blank" rel="noopener noreferrer" className="footer-link">Flying Turtle</a>
+      <a href="https://discord.gg/UtFxQbAEP3" target="_blank" rel="noopener noreferrer" className="footer-link">Discord</a>
     </footer>
   )
 }
